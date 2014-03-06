ffc8f34abdf88e4efce2bcc64a07d484968e9367
ScrRa <jesus@squad.com.mx>
2014-03-06 13:16:53 -0600

    Adding variables

---
+++ Code/KSPM/Network/Common/Information/IStatusPrintable.cs
@@ -0,0 +1,7 @@
+﻿namespace KSPM.Network.Common.Information
+{
+    public interface IStatusPrintable
+    {
+        void WriteStatus(KSPM.IO.Logging.Log output);
+    }
+}
---
+++ Code/KSPM/Network/Common/Information/Status.cs
@@ -0,0 +1,13 @@
+﻿using KSPM.IO.Logging;
+
+namespace KSPM.Network.Common.Information
+{
+    public abstract class Status
+    {
+        public enum SystemStatus : byte { None, Running, Stoped };
+
+        protected SystemStatus currentStatus;
+
+        public abstract void WriteToLog(Log output);
+    }
+}
