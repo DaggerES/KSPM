772718f202a3a7db8e3a4943fda53dae2e5a2fc7
Chuchito_SquadLapBeta <jesus@squad.com.mx>
2014-03-31 00:04:17 -0600

    Checking the cyclical buffer

---
+++ Code/KSPM/IO/Logging/DiagnosticsLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KSPM.IO.Logging
+{
+    public class DiagnosticsLog : FileLog
+    {
+        public DiagnosticsLog(string logFileName, bool isBinaryLog)
+            : base(logFileName, isBinaryLog)
+        {
+        }
+
+        public override void WriteTo(string message)
+        {
+            if (this.binaryLogEnabled)
+            {
+                this.logBinaryWriter.Write(message);
+                this.logBinaryWriter.Flush();
+            }
+            else
+            {
+                this.logTextWriter.WriteLine(message);
+                this.logTextWriter.Flush();
+            }
+        }
+    }
+}
