0dcf2a829dc7dfa0ff698bcf4cacd7485ee20471
ScrRa <jesus@squad.com.mx>
2014-04-04 10:48:42 -0600

    Starting to use SocketAsyncEventArgs because creating each time a IAsyncResult consumes a lot memory on apps with high performance.

---
+++ Code/KSPM/Network/Common/SocketAsyncEventArgsPool.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+
+namespace KSPM.Network.Common
+{
+    public class SocketAsyncEventArgsPool
+    {
+        protected System.Collections.Generic.Queue<SocketAsyncEventArgs> availableSAEA;
+        protected uint availableSlots;
+
+        public SocketAsyncEventArgsPool( uint initialCapacity )
+        {
+            this.availableSlots = initialCapacity;
+            this.availableSAEA = new System.Collections.Generic.Queue<SocketAsyncEventArgs>((int)this.availableSlots);
+        }
+
+        protected void InitializeSlots()
+        {
+            SocketAsyncEventArgs item = null;
+            for( int i = 0 ; i < this.availableSlots ; i++ )
+            {
+                item = new SocketAsyncEventArgs();
+                this.availableSAEA.Enqueue(item);
+            }
+        }
+
+        public SocketAsyncEventArgs NextSlot
+        {
+            get
+            {
+                lock (this.availableSAEA)
+                {
+                    if (this.availableSAEA.Count > 0)
+                    {
+                        return this.availableSAEA.Dequeue();
+                    }
+                    else
+                    {
+                        ///This should not happen.
+                        SocketAsyncEventArgs extraItem = new SocketAsyncEventArgs();
+                        return extraItem;
+                    }
+                }
+            }
+        }
+
+        public void Recycle(SocketAsyncEventArgs oldSocketAsyncEventArgs)
+        {
+            oldSocketAsyncEventArgs.AcceptSocket = null;
+            lock (this.availableSAEA)
+            {
+                this.availableSAEA.Enqueue(oldSocketAsyncEventArgs);
+            }
+        }
+    }
+}
