e6717a6e16e3b33fa4a78d6418ce5f9bcd6cc687
Chuchito <jesus@squad.com.mx>
2014-05-07 19:27:04 -0500

    Started to develop a port on unity.

--- Code/Unity/KSPMUnity/Assets/_Scripts/KSPM/Globals/UnityGlobals.cs
+++ Code/Unity/KSPMUnity/Assets/_Scripts/KSPM/Globals/UnityGlobals.cs
@@ -1,18 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using System.IO;
+
 public class UnityGlobals : MonoBehaviour
 {
-    public static string IOSwapPath = System.Environment.CurrentDirectory;
-    public static string SwapFolder = "swap";
+    /// <summary>
+    /// Enum to say which folder is going to be used.
+    /// </summary>
+    public enum WorkingMode : byte
+    {
+        None = 0,
+        Server,
+        Client
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string ServerIOFolder = "ServerConfiguration";
+    public string ClientIOFolder = "ClientConfiguration";
+
+    public WorkingMode workingMode = WorkingMode.None;
+
+    /// <summary>
+    /// Path to the working directory used to work.
+    /// </summary>
+    public static string WorkingDirectory;
+
+    void Awake()
+    {
+        switch (this.workingMode)
+        {
+            case WorkingMode.Server:
+                UnityGlobals.WorkingDirectory = string.Format("{0}{1}{2}{3}", Application.dataPath, Path.DirectorySeparatorChar, ServerIOFolder, Path.DirectorySeparatorChar);
+                break;
+            case WorkingMode.Client:
+                UnityGlobals.WorkingDirectory = string.Format("{0}{1}{2}{3}", Application.dataPath, Path.DirectorySeparatorChar, ClientIOFolder, Path.DirectorySeparatorChar);
+                break;
+            case WorkingMode.None:
+                Debug.LogWarning("Not working mode assigned");
+                break;
+        }
+        if (!Directory.Exists(UnityGlobals.WorkingDirectory))
+        {
+            Debug.Log(string.Format("Directory does not exists [\"{0}\"], creating directory.", UnityGlobals.WorkingDirectory));
+            try
+            {
+                Directory.CreateDirectory(UnityGlobals.WorkingDirectory);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+    }
 }
